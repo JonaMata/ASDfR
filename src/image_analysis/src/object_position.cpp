220640eaa6363633650cb12c5ea99e08fbe0977a
lieuweberg <36763108+lieuweberg@users.noreply.github.com>
2026-02-13 11:47:05 +0100

    easier object position

--- src/image_analysis/src/object_position.cpp
+++ src/image_analysis/src/object_position.cpp
@@ -10,66 +10,56 @@
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
-class ObjectPosition : public rclcpp::Node
-{
-  public:
+class ObjectPosition : public rclcpp::Node {
+public:
     ObjectPosition()
-    : Node("object_position")
-    {
-      this->declare_parameter<int>("threshold", 128);
-      subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
-      "image", 10, std::bind(&ObjectPosition::topic_callback, this, _1));
-      publisher_ = this->create_publisher<geometry_msgs::msg::Point>("object_position", 10);
+        : Node("object_position") {
+        this->declare_parameter<int>("threshold", 128);
+        subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+            "image", 10, std::bind(&ObjectPosition::topic_callback, this, _1));
+        publisher_ = this->create_publisher<geometry_msgs::msg::Point>("object_position", 10);
     }
 
-  private:
-    void topic_callback(const sensor_msgs::msg::Image & msg) const
-      {
+private:
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+    rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr publisher_;
+
+    void topic_callback(const sensor_msgs::msg::Image& msg) const {
         RCLCPP_INFO(this->get_logger(), "Received Image");
         int total_pixels = msg.width * msg.height;
-        bool thresholded[total_pixels];
+        // bool thresholded[total_pixels];
         int threshold = this->get_parameter("threshold").as_int();
 
-        for (int i = 0; i < total_pixels; i++) {
-            uint8_t grayscale_value = (msg.data[i*3] + msg.data[i*3 + 1] + msg.data[i*3 + 2]) / 3;
-            thresholded[i] = grayscale_value > threshold ? true : false;
-        }
-
-        int sum_y = 0;
-        int msum_y = 0;
+        int total_above_threshold = 0;
         int sum_x = 0;
-        int msum_x = 0;
+        int sum_y = 0;
 
-        for (int y = 0; y < msg.height; y++) {
-            for (int x = 0; x < msg.width; x++) {
-                int idx = y * msg.width + x;
-                if (thresholded[idx]) {
-                    sum_y++;
-                    msum_y += y+1;
-                    sum_x++;
-                    msum_x += x+1;
-                }
+        for (int i = 0; i < total_pixels; i++) {
+            if ((msg.data[i * 3] + msg.data[i * 3 + 1] + msg.data[i * 3 + 2]) / 3 > threshold) {
+                total_above_threshold++;
+                sum_x += i % msg.width + 1;
+                sum_y += i / msg.height + 1;
             }
         }
 
-        int y = sum_y > 0 ? msum_y / sum_y : 0;
-        int x = sum_x > 0 ? msum_x / sum_x : 0;
-
+        int x = 0;
+        int y = 0;
+        if (total_above_threshold != 0) {
+            x = sum_x / total_above_threshold - 1;
+            y = sum_y / total_above_threshold - 1;
+        }
 
         auto message = geometry_msgs::msg::Point();
         message.x = x;
         message.y = y;
         RCLCPP_INFO(this->get_logger(), "Publishing: x='%d', y='%d'", x, y);
         publisher_->publish(message);
-      }
-      rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
-      rclcpp::Publisher<geometry_msgs::msg::Point>::SharedPtr publisher_;
+    }
 };
 
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<ObjectPosition>());
-  rclcpp::shutdown();
-  return 0;
+int main(int argc, char* argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<ObjectPosition>());
+    rclcpp::shutdown();
+    return 0;
 }
