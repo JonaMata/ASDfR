c32425f379d02fc99ef2cbfc4e7e76ecb0d8558d
lieuweberg <36763108+lieuweberg@users.noreply.github.com>
2026-04-15 16:14:19 +0200

    ermsource /opt/ros/jazzy/setup.bash

--- ass3.3/object_position/src/object_position.cpp
+++ ass3.3/object_position/src/object_position.cpp
@@ -69,7 +69,7 @@ private:
         cv::cvtColor(frame, frame_hsv, cv::COLOR_BGR2HSV);
 
         cv::Mat mask;
-        cv::inRange(frame_hsv, cv::Scalar(30, 85, 10), cv::Scalar(75, 255, 255), mask);
+        cv::inRange(frame_hsv, cv::Scalar(30, 85, 30), cv::Scalar(75, 255, 255), mask);
 
         cv::morphologyEx(mask, mask, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
         cv::morphologyEx(mask, mask, cv::MORPH_DILATE, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(25, 25)));
