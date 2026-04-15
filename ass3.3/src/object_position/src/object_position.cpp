1006e603a00f56fad5cb99638a2e031c9d5663b7
lieuweberg <36763108+lieuweberg@users.noreply.github.com>
2026-04-15 17:31:07 +0200

    🦕

--- ass3.3/src/object_position/src/object_position.cpp
+++ ass3.3/src/object_position/src/object_position.cpp
@@ -30,10 +30,6 @@ using std::placeholders::_1;
 class ObjectPosition : public rclcpp::Node {
 public:
     ObjectPosition() : Node("object_position") {
-        lightness_lower = this->declare_parameter<int>("lightness_lower", 30);
-        lightness_upper = this->declare_parameter<int>("lightness_upper", 200);
-        mask_lightness_upper = this->declare_parameter<int>("mask_lightness_upper", 80);
-        mask_lightness_lower = this->declare_parameter<int>("mask_lightness_lower", 30);
         threshold = this->declare_parameter<int>("threshold", 10);
         fromCenter = this->declare_parameter<bool>("from_center", false);
         showMask = declare_parameter<bool>("show_mask", false);
@@ -48,10 +44,9 @@ private:
 
     bool fromCenter;
     int threshold;
-    int lightness_lower, lightness_upper, mask_lightness_lower, mask_lightness_upper;
     bool showMask;
     mutable cv::Ptr<cv::SimpleBlobDetector> blobDetector;
-    
+
     void topic_callback(const sensor_msgs::msg::Image& msg) const {
         if (blobDetector == nullptr) {
             cv::SimpleBlobDetector::Params params;
@@ -69,27 +64,19 @@ private:
             params.minInertiaRatio = 0.5;
             blobDetector = cv::SimpleBlobDetector().create(params);
         }
-        
+
         auto frame = cv_bridge::toCvCopy(msg, "bgr8")->image;
         cv::Mat frame_hsv;
         cv::cvtColor(frame, frame_hsv, cv::COLOR_BGR2HSV);
-        
-        std::vector<cv::Mat> channels;
-        cv::split(frame_hsv, channels);
-        int lightness = (int)cv::mean(channels[2])[0];
-        
-        lightness = std::clamp(lightness, lightness_lower, lightness_upper);
-        int mask_lightness = mask_lightness_upper - (lightness - lightness_lower) / (lightness_upper - lightness_lower) * (mask_lightness_upper - mask_lightness_lower);
-        RCLCPP_INFO(this->get_logger(), "Lightness=%d, masklightness=%d", lightness, mask_lightness);
-        
+
         cv::Mat mask;
-        cv::inRange(frame_hsv, cv::Scalar(30, 85, mask_lightness), cv::Scalar(85, 255, 255), mask);
-        
+        cv::inRange(frame_hsv, cv::Scalar(30, 85, 80), cv::Scalar(85, 255, 255), mask);
+
         cv::morphologyEx(mask, mask, cv::MORPH_OPEN, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
         cv::morphologyEx(mask, mask, cv::MORPH_DILATE, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(25, 25)));
         cv::morphologyEx(mask, mask, cv::MORPH_ERODE, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(10, 10)));
         cv::morphologyEx(mask, mask, cv::MORPH_ERODE, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)));
-        
+
         std::vector<cv::KeyPoint> blobs;
         blobDetector->detect(mask, blobs);
 
@@ -103,7 +90,7 @@ private:
                 biggestBlobCenterY = blob->pt.y;
             }
         }
-        
+
         if (showMask) {
             cv::Mat result;
             cv::bitwise_and(frame, frame, result, mask);
