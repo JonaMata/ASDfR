7d3039a7872f0a042b7f51a84b89a136fc1d092f
Jonathan Matarazzi <git@jonathanm.nl>
2026-04-16 16:39:12 +0200

    rename folders

--- ass3.4/src/control_loop/src/control_loop.cpp
+++ ass3.4/src/control_loop/src/control_loop.cpp
@@ -2,10 +2,9 @@
 
 ControlLoop::ControlLoop(uint write_decimator_freq, uint monitor_freq) :
     XenoFrt20Sim(write_decimator_freq, monitor_freq, file, &data_to_be_logged),
-    file(1,"./xrf2_logging/TEMPLATE","bin"), // change template to your project name
-    controller()
-{
-     printf("%s: Constructing rampio\n", __FUNCTION__);
+    file(1, "./xrf2_logging/TEMPLATE", "bin"), // change template to your project name
+    controller() {
+    printf("%s: Constructing rampio\n", __FUNCTION__);
     // Add variables to logger to be logged, has to be done before you can log data
     logger.addVariable("set_steer_left", double_);
     logger.addVariable("set_steer_right", double_);
@@ -16,13 +15,11 @@ ControlLoop::ControlLoop(uint write_decimator_freq, uint monitor_freq) :
     controller.SetFinishTime(0.0);
 }
 
-ControlLoop::~ControlLoop()
-{
-    
+ControlLoop::~ControlLoop() {
+
 }
 
-int ControlLoop::initialising()
-{
+int ControlLoop::initialising() {
     // Set physical and cyber system up for use in a 
     // Return 1 to go to initialised state
 
@@ -36,11 +33,13 @@ int ControlLoop::initialising()
     prevChannel1 = sample_data.channel1;
     prevChannel2 = sample_data.channel2;
 
+    u[2] = 0;
+    u[3] = 0;
+
     return 1;
 }
 
-int ControlLoop::initialised()
-{
+int ControlLoop::initialised() {
     // Keep the physical syste in a state to be used in the run state
     // Call start() or return 1 to go to run state
 
@@ -49,13 +48,12 @@ int ControlLoop::initialised()
     return 1;
 }
 
-int ControlLoop::run()
-{
+int ControlLoop::run() {
     // Do what you need to do
     // Return 1 to go to stopping state
 
     // Start logger
-    logger.start();                             
+    logger.start();
     monitor.printf("Hello from run\n");
 
     int channel1 = sample_data.channel1;
@@ -64,16 +62,16 @@ int ControlLoop::run()
     int diff1 = channel1 - prevChannel1;
     int diff2 = channel2 - prevChannel2;
 
-    if(abs(diff1) > 7000) {
-        if(diff1>0) {
+    if (abs(diff1) > 7000) {
+        if (diff1 > 0) {
             diff1 -= 16384;
         } else {
             diff1 += 16384;
         }
     }
 
-    if(abs(diff2) > 7000) {
-        if(diff2>0) {
+    if (abs(diff2) > 7000) {
+        if (diff2 > 0) {
             diff2 -= 16384;
         } else {
             diff2 += 16384;
@@ -105,21 +103,20 @@ int ControlLoop::run()
 
     actuate_data.pwm1 = pwm1;
     actuate_data.pwm2 = pwm2;
-    
+
     // //  Change some data for logger            
     data_to_be_logged.setSteerRight = u[2];
     data_to_be_logged.setSteerLeft = u[3];
     data_to_be_logged.posRight = u[0];
     data_to_be_logged.posLeft = u[1];
-    if(controller.IsFinished())
+    if (controller.IsFinished())
         return 1;
 
 
     return 0;
 }
 
-int ControlLoop::stopping()
-{
+int ControlLoop::stopping() {
     // Bring the physical system to a stop and set it in a state that the system can be deactivated
     // Return 1 to go to stopped state
     logger.stop();                                // Stop logger
@@ -128,8 +125,7 @@ int ControlLoop::stopping()
     return 1;
 }
 
-int ControlLoop::stopped()
-{
+int ControlLoop::stopped() {
     // A steady state in which the system can be deactivated whitout harming the physical system
 
     monitor.printf("Hello from stopping\n");          // Do something
@@ -137,24 +133,21 @@ int ControlLoop::stopped()
     return 0;
 }
 
-int ControlLoop::pausing()
-{
+int ControlLoop::pausing() {
     // Bring the physical system to a stop as fast as possible without causing harm to the physical system
 
     evl_printf("Hello from pausing\n");           // Do something
-    return 1 ;
+    return 1;
 }
 
-int ControlLoop::paused()
-{
+int ControlLoop::paused() {
     // Keep the physical system in the current physical state
 
     monitor.printf("Hello from paused\n");            // Do something
     return 0;
 }
 
-int ControlLoop::error()
-{
+int ControlLoop::error() {
     // Error detected in the system 
     // Can go to error if the previous state returns 1 from every other state function but initialising 
 
